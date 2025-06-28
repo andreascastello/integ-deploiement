d103774fc9788b478a5208408f25cfa7b7cef8a7
andreascastello <andreas.castello06@gmail.com>
2025-06-28 17:23:01 +0200

    final

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000/integ-deploiement/',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
   },
